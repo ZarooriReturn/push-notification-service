3f4189c9f7822ca6e897fa86e19a7f24e75367f4
Zaroori Return <www.hackertechyt@gmail.com>
2024-09-16 23:51:58 -0700

    Update send-notification.js

--- api/send-notification.js
+++ api/send-notification.js
@@ -22,6 +22,7 @@ export default async function handler(req, res) {
                 message: message,
                 target_url: target_url,
                 image: image,
+                user_uid: userUid,
                 action_buttons: action_buttons
             }, {
                 headers: {
